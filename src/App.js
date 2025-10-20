4957b4fd23260556afd3a94cc48e03cb3a126b44
Junepil Lee <hanu9257@gmail.com>
2025-10-20 12:00:34 +0800

    feat: implement number addition function

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ class App {
   async run() {
     const inputString = await MissionUtils.Console.readLineAsync();
     const numbers = this.parseInput(inputString);
+    const result = this.sumNumbers(numbers);
   }
-  
+
+  sumNumbers(numbers) {
+    let sum = 0;
+
+    for (const num in numbers) {
+      sum += num;
+    }
+
+    return sum;
+  }
+
   parseInput(inputString) {
-    console.log(inputString)
+    console.log(inputString);
     let numbers = null;
     const baseSpliters = ",;";
 
     let customSpliter = null;
 
     if (inputString.includes("\n")) {
-      const [configString, data] = inputString.split("\n")
+      const [configString, data] = inputString.split("\n");
       const spliterRegex = /\/\/(.)/;
-  
+
       customSpliter = configString.match(spliterRegex)[0];
-      const spliters = new RegExp(baseSpliters + customSpliter)
-      numbers = data.split(spliters)
+      const spliters = new RegExp(baseSpliters + customSpliter);
+      numbers = data.split(spliters);
     } else {
-      numbers = inputString.split(baseSpliters)
-    } 
-    numbers.map(n => parseFloat(n));
+      numbers = inputString.split(baseSpliters);
+    }
+    numbers.map((n) => parseFloat(n));
   }
 }
 
